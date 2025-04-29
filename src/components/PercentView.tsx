9f62c66b24ee70f4306b4c0fa85a27a0ecc11c87
Vít Provazník <vit.provaznik.023@pslib.cz>
2025-04-29 18:14:22 +0200

    foramted with prettier

--- src/components/PercentView.tsx
+++ src/components/PercentView.tsx
@@ -11,17 +11,21 @@ const PercentView: FC<PercentViewType> = ({ max, value, makeColor }) => {
   // if (value > max) {
   //   value = max;
   // }
-  let percent = value/max;
+  let percent = value / max;
   return (
     <div
       className={style.percentview}
       style={{ color: makeColor(percent), fontSize: "2em" }}
     >
-      {percent*100 + "%"}
-      <div className={style.bar}
-      style={{backgroundColor: makeColor(percent), width: value/max *100 + "%", height: "20px"}}>
-
-      </div>
+      {percent * 100 + "%"}
+      <div
+        className={style.bar}
+        style={{
+          backgroundColor: makeColor(percent),
+          width: (value / max) * 100 + "%",
+          height: "20px",
+        }}
+      ></div>
     </div>
   );
 };
